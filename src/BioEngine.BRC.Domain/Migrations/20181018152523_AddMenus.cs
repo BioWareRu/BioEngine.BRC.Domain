f3acbd9033da6073e8b519e271976ea00aa527df
SonicGD <sonicgd@gmail.com>
2018-10-23 21:20:51 +0500

    Add menu migration

---
+++ src/BioEngine.BRC.Domain/Migrations/20181018152523_AddMenus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace BioEngine.BRC.Domain.Migrations
+{
+    public partial class AddMenus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Menus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DateAdded = table.Column<DateTimeOffset>(nullable: false),
+                    DateUpdated = table.Column<DateTimeOffset>(nullable: false),
+                    IsPublished = table.Column<bool>(nullable: false),
+                    DatePublished = table.Column<DateTimeOffset>(nullable: true),
+                    SiteIds = table.Column<int[]>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Items = table.Column<string>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menus", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Menus");
+        }
+    }
+}
