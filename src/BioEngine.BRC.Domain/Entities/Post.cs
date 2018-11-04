8a327d9c2a324c4c989b4dccec4d90d783c1be89
SonicGD <sonicgd@gmail.com>
2018-11-04 20:36:41 +0500

    Post now can split text by [cut] separator

--- src/BioEngine.BRC.Domain/Entities/Post.cs
+++ src/BioEngine.BRC.Domain/Entities/Post.cs
@@ -1,4 +1,5 @@
-﻿using BioEngine.Core.Entities;
+﻿using System;
+using BioEngine.Core.Entities;
 using BioEngine.Core.Interfaces;
 
 namespace BioEngine.BRC.Domain.Entities
@@ -6,6 +7,34 @@ namespace BioEngine.BRC.Domain.Entities
     public class Post : ContentItem<PostData>
     {
         public override string TypeTitle { get; set; } = "Пост";
+
+        private (string shortText, string longText)? _texts;
+
+        public string GetShortText()
+        {
+            ParseText();
+            return _texts?.shortText;
+        }
+
+        private void ParseText()
+        {
+            if (_texts != null) return;
+            if (!string.IsNullOrEmpty(Data?.Text) && Data.Text.Contains("[cut]"))
+            {
+                var parts = Data.Text.Split(new[] {"[cut]"}, StringSplitOptions.RemoveEmptyEntries);
+                _texts = (parts[0], parts[1]);
+            }
+            else
+            {
+                _texts = (Data?.Text, null);
+            }
+        }
+
+        public string GetFullText()
+        {
+            ParseText();
+            return _texts?.longText;
+        }
     }
 
     public class PostData : TypedData
