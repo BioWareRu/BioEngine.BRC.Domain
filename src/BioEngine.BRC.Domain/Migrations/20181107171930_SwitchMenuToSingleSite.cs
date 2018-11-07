1555a554375a7890c6270b949799b693a6a2a63b
SonicGD <sonicgd@gmail.com>
2018-11-07 22:45:49 +0500

    Switch menu to SingleSiteEntity

---
+++ src/BioEngine.BRC.Domain/Migrations/20181107171930_SwitchMenuToSingleSite.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BioEngine.BRC.Domain.Migrations
+{
+    public partial class SwitchMenuToSingleSite : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SiteIds",
+                table: "Menus");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SiteId",
+                table: "Menus",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SiteId",
+                table: "Menus");
+
+            migrationBuilder.AddColumn<int[]>(
+                name: "SiteIds",
+                table: "Menus",
+                nullable: true);
+        }
+    }
+}
