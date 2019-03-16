e8df8842738a4731969450f0605e45e06b3c942f
George <sonicgd@gmail.com>
2019-03-16 13:19:51 +0500

    Implement posts versions

---
+++ src/BioEngine.BRC.Domain/Migrations/20190316074453_add_post_versioning.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BioEngine.BRC.Domain.Migrations
+{
+    public partial class add_post_versioning : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PostVersions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DateAdded = table.Column<DateTimeOffset>(nullable: false),
+                    DateUpdated = table.Column<DateTimeOffset>(nullable: false),
+                    IsPublished = table.Column<bool>(nullable: false),
+                    DatePublished = table.Column<DateTimeOffset>(nullable: true),
+                    PostId = table.Column<Guid>(nullable: false),
+                    Data = table.Column<string>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostVersions", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostVersions");
+        }
+    }
+}
