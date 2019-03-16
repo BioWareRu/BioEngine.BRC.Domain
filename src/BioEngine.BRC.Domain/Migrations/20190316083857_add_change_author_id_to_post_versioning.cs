c4174754f68fde4bdb802df22cd20a479701bea7
George <sonicgd@gmail.com>
2019-03-16 13:42:00 +0500

    Save user id to version

---
+++ src/BioEngine.BRC.Domain/Migrations/20190316083857_add_change_author_id_to_post_versioning.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BioEngine.BRC.Domain.Migrations
+{
+    public partial class add_change_author_id_to_post_versioning : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ChangeAuthorId",
+                table: "PostVersions",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChangeAuthorId",
+                table: "PostVersions");
+        }
+    }
+}
