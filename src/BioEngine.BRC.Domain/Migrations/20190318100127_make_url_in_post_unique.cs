f2fbfe35f1b3c517bff7094df1f61ea8879d8701
George <sonicgd@gmail.com>
2019-03-18 17:54:59 +0500

    Make posts url unique. Validate.

---
+++ src/BioEngine.BRC.Domain/Migrations/20190318100127_make_url_in_post_unique.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BioEngine.BRC.Domain.Migrations
+{
+    public partial class make_url_in_post_unique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_Url",
+                table: "Posts");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_Url",
+                table: "Posts",
+                column: "Url",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_Url",
+                table: "Posts");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_Url",
+                table: "Posts",
+                column: "Url");
+        }
+    }
+}
