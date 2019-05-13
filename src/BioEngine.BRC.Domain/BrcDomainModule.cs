14fbdca24d434f8440e30a5ad370755a3c1ee12a
George <sonicgd@gmail.com>
2019-05-13 15:09:18 +0500

    Use new modules

---
+++ src/BioEngine.BRC.Domain/BrcDomainModule.cs
@@ -0,0 +1,18 @@
+using BioEngine.Core.API;
+using BioEngine.Core.Modules;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BioEngine.BRC.Domain
+{
+    public class BrcDomainModule : BioEngineModule
+    {
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration,
+            IHostEnvironment environment)
+        {
+            base.ConfigureServices(services, configuration, environment);
+            services.RegisterApiEntities(GetType().Assembly);
+        }
+    }
+}
