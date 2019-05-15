c2e9c8eb6c3cc18343bbd084d58fa55aceeb3a59
George <sonicgd@gmail.com>
2019-05-15 12:48:48 +0500

    Rearrange app startups.

--- src/BioEngine.BRC.Site/BrcSiteStartup.cs
+++ src/BioEngine.BRC.Site/BrcSiteStartup.cs
@@ -1,15 +1,17 @@
+using System.Globalization;
 using BioEngine.Core.Logging.Controllers;
 using BioEngine.Core.Site;
 using BioEngine.Extra.Ads.Site;
 using BioEngine.Extra.IPB.Controllers;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace BioEngine.BRC.Site
 {
-    public abstract class BrcSiteStartup : BioEngineStartup
+    public abstract class BrcSiteStartup : BioEngineSiteStartup
     {
         private readonly IHostEnvironment _environment;
 
@@ -18,27 +20,38 @@ namespace BioEngine.BRC.Site
             _environment = environment;
         }
 
-        public override void ConfigureServices(IServiceCollection services)
+        protected override IMvcBuilder ConfigureMvc(IMvcBuilder mvcBuilder)
         {
-            base.ConfigureServices(services);
-            services
-                .AddControllersWithViews()
-                .AddApplicationPart(typeof(LogsController).Assembly)
+            base.ConfigureMvc(mvcBuilder).AddApplicationPart(typeof(LogsController).Assembly)
                 .AddApplicationPart(typeof(UserController).Assembly)
                 .AddApplicationPart(typeof(BrcSiteModule).Assembly)
                 .AddApplicationPart(typeof(AdsSiteController).Assembly);
             if (_environment.IsDevelopment())
             {
-                services.AddControllersWithViews().AddRazorRuntimeCompilation();
+                mvcBuilder.AddRazorRuntimeCompilation();
             }
+
+            return mvcBuilder;
         }
 
-        protected override void ConfigureApp(IApplicationBuilder app, IHostEnvironment env)
+        protected override void ConfigureAfterRouting(IApplicationBuilder app, IHostEnvironment env)
         {
-            base.ConfigureApp(app, env);
-
+            base.ConfigureAfterRouting(app, env);
             app.UseAuthentication();
             app.UseAuthorization();
+        }
+
+        protected override void ConfigureBeforeRouting(IApplicationBuilder app, IHostEnvironment env)
+        {
+            base.ConfigureBeforeRouting(app, env);
+            var supportedCultures = new[] {new CultureInfo("ru-RU"), new CultureInfo("ru")};
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("ru-RU"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
         }
     }
 }
