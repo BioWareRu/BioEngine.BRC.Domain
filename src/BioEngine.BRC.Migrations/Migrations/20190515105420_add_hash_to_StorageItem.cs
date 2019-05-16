40c761ee50a592a38febf2f747385d2e574f9646
George <sonicgd@gmail.com>
2019-05-16 16:46:01 +0500

    Add hash to storage items and implement deduplication

---
+++ src/BioEngine.BRC.Migrations/Migrations/20190515105420_add_hash_to_StorageItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BioEngine.BRC.Migrations.Migrations
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public partial class add_hash_to_StorageItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                "Hash",
+                "StorageItems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                "Hash",
+                "StorageItems");
+        }
+    }
+}
