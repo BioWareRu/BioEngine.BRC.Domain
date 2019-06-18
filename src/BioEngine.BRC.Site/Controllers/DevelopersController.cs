ecb982049e354c377da119a6c4fca6ec00800e20
George <sonicgd@gmail.com>
2019-06-18 18:05:12 +0500

    Fix content actions

--- src/BioEngine.BRC.Site/Controllers/DevelopersController.cs
+++ src/BioEngine.BRC.Site/Controllers/DevelopersController.cs
@@ -1,29 +1,27 @@
 using System.Threading.Tasks;
 using BioEngine.BRC.Domain.Entities;
 using BioEngine.BRC.Domain.Repository;
-using BioEngine.Core.Repository;
 using BioEngine.Core.Site;
 using BioEngine.Core.Web;
+using BioEngine.Posts.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BioEngine.BRC.Site.Controllers
 {
     public class DevelopersController : SectionController<Developer, DevelopersRepository>
     {
-        public DevelopersController(
-            BaseControllerContext<Developer, DevelopersRepository> context,
-            ContentItemsRepository contentItemsRepository) : base(context, contentItemsRepository)
+        public DevelopersController(BaseControllerContext<Developer, DevelopersRepository> context) : base(context)
         {
         }
 
         public Task<IActionResult> PostsAsync(string url)
         {
-            return base.PostsAsync(new[] {"post"}, url);
+            return ShowContentAsync<Post>(url);
         }
 
         public Task<IActionResult> PostsPageAsync(string url, int page)
         {
-            return base.PostsPageAsync(new[] {"post"}, url, page);
+            return ShowContentAsync<Post>(url, page);
         }
     }
 }
