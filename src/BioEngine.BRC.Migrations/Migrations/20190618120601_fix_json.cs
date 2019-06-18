4e3951286356a0a39821583aa6adecba94571f52
George <sonicgd@gmail.com>
2019-06-18 17:58:01 +0500

    Fix wrong json conversion, need arrays in db

---
+++ src/BioEngine.BRC.Migrations/Migrations/20190618120601_fix_json.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BioEngine.BRC.Migrations.Migrations
+{
+    public partial class fix_json : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"ALTER TABLE ""Content"" ALTER COLUMN ""TagIds"" TYPE uuid[] USING string_to_array(translate(""TagIds""::text, '[]\"" ', ''), ',')::uuid[];");
+            migrationBuilder.Sql(
+                @"ALTER TABLE ""Content"" ALTER COLUMN ""SectionIds"" TYPE uuid[] USING string_to_array(translate(""SectionIds""::text, '[]\"" ', ''), ',')::uuid[];");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Content\" ALTER COLUMN \"TagIds\" TYPE jsonb USING to_jsonb(\"TagIds\")");
+            migrationBuilder.Sql(
+                "ALTER TABLE \"Content\" ALTER COLUMN \"SectionIds\" TYPE jsonb USING to_jsonb(\"SectionIds\")");
+        }
+    }
+}
