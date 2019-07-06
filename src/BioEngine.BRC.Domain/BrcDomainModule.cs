088d30f3538bd5037c5695a1a31af4c76cd1d6bf
George <sonicgd@gmail.com>
2019-07-06 12:49:11 +0500

    Register search providers

--- src/BioEngine.BRC.Domain/BrcDomainModule.cs
+++ src/BioEngine.BRC.Domain/BrcDomainModule.cs
@@ -1,8 +1,25 @@
+using BioEngine.BRC.Domain.Entities;
+using BioEngine.BRC.Domain.Search;
 using BioEngine.Core.Modules;
+using BioEngine.Core.Search;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace BioEngine.BRC.Domain
 {
     public class BrcDomainModule : BaseBioEngineModule
     {
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration,
+            IHostEnvironment environment)
+        {
+            base.ConfigureServices(services, configuration, environment);
+            services.RegisterSearchRepositoryHook<Developer>()
+                .RegisterSearchProvider<DevelopersSearchProvider, Developer>();
+            services.RegisterSearchRepositoryHook<Game>()
+                .RegisterSearchProvider<GamesSearchProvider, Game>();
+            services.RegisterSearchRepositoryHook<Topic>()
+                .RegisterSearchProvider<TopicsSearchProvider, Topic>();
+        }
     }
 }
