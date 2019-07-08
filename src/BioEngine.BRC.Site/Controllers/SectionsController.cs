e20428ff7fd48dae0fa3fb28bef93577a8959ee6
George <sonicgd@gmail.com>
2019-07-08 17:17:05 +0500

    Add section controller

---
+++ src/BioEngine.BRC.Site/Controllers/SectionsController.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BioEngine.BRC.Domain.Repository;
+using BioEngine.Core.Entities;
+using BioEngine.Core.Site;
+using BioEngine.Core.Site.Model;
+using BioEngine.Core.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BioEngine.BRC.Site.Controllers
+{
+    public class SectionsController : BaseSiteController
+    {
+        private readonly GamesRepository _gamesRepository;
+        private readonly DevelopersRepository _developersRepository;
+
+        public SectionsController(BaseControllerContext context, GamesRepository gamesRepository,
+            DevelopersRepository developersRepository) : base(context)
+        {
+            _gamesRepository = gamesRepository;
+            _developersRepository = developersRepository;
+        }
+
+        [HttpGet("/games.html")]
+        public async Task<IActionResult> ListGamesAsync()
+        {
+            var games = await _gamesRepository.GetAllAsync();
+            return ListSections(games.items, "Игры");
+        }
+
+        [HttpGet("/developers.html")]
+        public async Task<IActionResult> ListDevelopersAsync()
+        {
+            var developers = await _developersRepository.GetAllAsync();
+            return ListSections(developers.items, "Компании");
+        }
+
+        private IActionResult ListSections(IEnumerable<Section> sections, string title)
+        {
+            var sectionsList = new Dictionary<char, List<Section>>();
+            foreach (var section in sections.OrderBy(s => s.Title))
+            {
+                var firstLetter = section.Title.ToCharArray().First();
+                if (!sectionsList.ContainsKey(firstLetter))
+                {
+                    sectionsList.Add(firstLetter, new List<Section>());
+                }
+
+                sectionsList[firstLetter].Add(section);
+            }
+
+            return View("Index",
+                new PageViewModel<Dictionary<char, List<Section>>>(GetPageContext(), sectionsList) {Title = title});
+        }
+    }
+}
